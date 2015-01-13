e6e371d2f40c3c3c615b55c86d1ae991ec5df6db
daiz <da.iz.mg@gmail.com>
2015-01-13 22:46:02 +0900

    bugfix #1

--- app_js/griddles-app-card-clicked/griddles-app-card-clicked-2.js
+++ app_js/griddles-app-card-clicked/griddles-app-card-clicked-2.js
@@ -1,12 +1,18 @@
 //// griddles-app-card-clicked-2.js
 //// Generated in 2015-01-12 18:24:11.
 function griddlesAppCardClicked(card) {
-   /* open web page */
    changeHeadImage(card[0].src);
+   /** 
+   miil側の仕様により、http://miil.me/g/ で始まるurlではうまく
+   webintentが機能しない（ミイルアプリと連携されない）ことがあるため、
+   /g/ を /p/ に置換して、http://miil.me/p/ にする必要がある。
+   */
+   var url = card[0].data.webpage;
+   url = url.replace(/\/g\//, '/p/');
    window.plugins.webintent.startActivity({
-        action: window.plugins.webintent.ACTION_VIEW,
-        url: card[0].data.webpage},
-        function() {},
-        function() {alert('Failed to open URL via Android Intent')}
-    );
+     action: window.plugins.webintent.ACTION_VIEW,
+     url: url},
+     function() {},
+     function() {alert('Failed to open URL via Android Intent')}
+   );
 }
