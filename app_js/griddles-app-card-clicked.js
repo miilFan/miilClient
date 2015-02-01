9268a3cfea31fa8cbd204bb418287e7d0cbe1b1e
daiz <da.iz.mg@gmail.com>
2015-02-01 11:01:54 +0900

    maint

--- app_js/griddles-app-card-clicked.js
+++ app_js/griddles-app-card-clicked.js
@@ -13,7 +13,7 @@ function griddlesAppCardClicked(card) {
 /** apricot.R = oss-aa **/
 function griddlesAppCardClicked(card) {
    changeHeadImage(card[0].src);
-   /** 
+   /**
    miil側の仕様により、http://miil.me/g/ で始まるurlではうまく
    webintentが機能しない（ミイルアプリと連携されない）ことがあるため、
    /g/ を /p/ に置換して、http://miil.me/p/ にする必要がある。
