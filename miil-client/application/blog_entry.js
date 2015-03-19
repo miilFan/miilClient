b75e484ab0f5791735d368db4de1ad6b2125b55d
daiki <dev.da.iz.0x@gmail.com>
2015-03-19 10:17:05 +0900

    ブログへのリンクを追加

--- miil-client/application/blog_entry.js
+++ miil-client/application/blog_entry.js
@@ -1,17 +1,34 @@
 var release_blog_entries = [
     {
-        text: ["バージョン 0.2.1", "block"],
+      text: ["公式 ミイルブログ", "block"],
+      src: "miil-client/assets/miil.jpg",
+      dataset: {
+        shadowZ: 0,
+        page: "http://blog.miil.me/"
+      }
+    },
+    {
+        text: ["miilClient v0.2.1 リリースブログ", "block"],
         src: "miil-client/assets/0.2.1.jpg",
-        dataset: {shadowZ: 0}
+        dataset: {
+          shadowZ: 0,
+          page: "http://daiiz.hatenablog.com/entry/2015/01/15/012624"
+        }
     },
     {
-        text: ["バージョン 0.2.0", "block"],
+        text: ["miilClient v0.2.0 リリースブログ", "block"],
         src: "miil-client/assets/0.2.0.jpg",
-        dataset: {shadowZ: 0}
+        dataset: {
+          shadowZ: 0,
+          page: "http://daiiz.hatenablog.com/entry/2015/01/05/003822"
+        }
     },
     {
-        text: ["バージョン 0.1.1", "block"],
+        text: ["miilClient v0.1.1 リリースブログ", "block"],
         src: "miil-client/assets/0.1.1.jpg",
-        dataset: {shadowZ: 0}
+        dataset: {
+          shadowZ: 0,
+          page: "http://daiiz.hatenablog.com/entry/2014/11/11/011106"
+        }
     }
 ];
