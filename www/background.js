2a2cdfd075d3c16f3c7b804eaa4390b831f30efd
daiiz <da.iz.mg@gmail.com>
2015-11-25 10:53:01 +0900

    横長ウィンドウ実験

--- www/background.js
+++ www/background.js
@@ -6,8 +6,8 @@
  */
 chrome.app.runtime.onLaunched.addListener(function(launchData) {
   chrome.app.window.create('index.html', {
-      width: 488,
-      height: 668,
+      width: 606, // 488,
+      height: 526, // 668,
       type: 'shell',
       singleton: false
    },function(appWindow) {
